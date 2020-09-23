8daabc0422e0fe62eda4e41b505f03842abe319d
Patrick Navarre <patrick.navarre@codeimmersives.com>
2020-09-23 15:01:38 -0400

    7 passed-to-be-cont

--- main.js
+++ main.js
@@ -2,9 +2,42 @@
  * YOUR CODE BELOW! *
  ********************/
 
+function moreThan5(num){
+  return num > 5;
+}
+let result = moreThan5(6)
+result;
 
+function isNewTopScore(score, topScore){
+  return score > topScore;
+}
+result = isNewTopScore(10)
+result;
 
+function isInDanger(num){
+  return num >= 60 && num <= 71;
+}
 
+function isCoasting(num){
+  return num >= 72 && num <= 83;
+}
+
+function isSucceeding(num){
+  return num >= 84 && num <= 92;
+}
+
+function isFailing(num){
+  return num < 60
+}
+
+function isAcing(num){
+  return num > 92
+}
+
+function isStudent(name){
+  return name = 'student'
+
+}
 
 /********************************************************************************************
  * CODE BELOW IS FOR EXPORTING THE VARIABLES AND FUNCTIONS YOU WROTE ABOVE TO MAIN.TEST.JS. *
